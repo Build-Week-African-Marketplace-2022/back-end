c8fc83d062bb13d98241f8cee3833db4d8351956
Drew Nelson <realseal177@gmail.com>
2022-03-02 10:19:44 -0800

    middleware and model and server done

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const authRouter = require('./auth/auth-router')
+const usersRouter = require('./users/user-router')
+const itemsRouter = require('./items/items-router')
+
+const { restricted } = require('./auth/auth-middleware')
+
+const { errorHandling } = require('./global-middleware')
+
+const server = express()
+
+server.use(express.json())
+server.use(helmet())
+server.use(cors())
+
+server.use('/api/auth', authRouter)
+server.use('/api/users', restricted, usersRouter)
+server.use('/api/items', restricted, itemsRouter)
+
+server.use(errorHandling)
+
+module.exports = server
